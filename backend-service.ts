3efb754b9c57cf671871c96f6b1f55834d4426e6
Andrey Selin <andreiprostoandrei@gmail.com>
2025-03-21 13:30:03 +0100

    Add nest backend service sample

---
+++ backend-service.ts
@@ -0,0 +1,115 @@
+/*
+ * Nest.JS service with top-level business logic for 
+ * buying subscription as non-authorized user (and as authorized later)
+ * Main method is createOrder
+ */
+
+@Injectable()
+export class CreateOrderService {
+  constructor(
+    private startSubscriptionBusinessService: StartSubscriptionBusinessService,
+    private purchasesCommercialService: PurchasesCommercialService,
+    private commonOtpLogicBusinessService: CommonOtpLogicBusinessService,
+  ) {}
+
+  private isPromoCodeValid(promoCode: string | null): boolean {
+    return promoCode === 'CODE1';
+  }
+
+  private async recalculateCart(cart: ExpectedCartStructure): Promise<ExpectedCartStructure> {
+    // This has to be recalculated and compared to passed cart
+    return cart;
+  }
+
+  private validateUserDetails(userDetails: UserDetails): null {
+    const validationResult = userDetailsScheme.validate(userDetails);
+    if (validationResult.error) {
+      throw new Exception2({
+        exceptionKey: 'invalidUserDetails',
+        data: { userDetails },
+      });
+    }
+    return null;
+  }
+
+  async createOrderAsUnauthorized({
+    email,
+    ...rest
+  }: CreateOrderAsUnauthorizedRequestParams) {
+    this.logger.info({ key: 'CreateOrderService.createOrderAsUnauthorized:start', details: { email } });
+
+    const { createdUserId } = await this.commonOtpLogicBusinessService
+      .createUserAndAuthMethodByEmailOrFail(email);
+
+    this.logger.info({ key: 'CreateOrderService.createOrderAsUnauthorized:userCreated', details: { createdUserId } });  
+
+    return this.createOrder({
+      userId: asNonNullish(createdUserId),
+      ...rest,
+    })
+  }
+
+  private async createOrder({
+    userId,
+    cart: cartToRecalculate,
+    userDetails,
+    promoCode,
+  }: CreateOrderUnifiedParams) {
+
+    this.logger.info({ key: 'CreateOrderService.createOrder:start', details: { userId, cart, userDetails, promoCode } });
+
+    /* * * * * * * * * * * * * * * */
+    /*                             */
+    /*   Checking and validation   */
+    /*                             */
+    /* * * * * * * * * * * * * * * */
+
+    this.validateUserDetails(userDetails);
+
+    // Recalculate cart to check it
+    const checkedCart = await this.recalculateCart(cartToRecalculate);
+
+    /* * * * * * * * * * * * */
+    /*                       */
+    /*   Init subscription   */
+    /*                       */
+    /* * * * * * * * * * * * */
+
+    const subscriptionTypeKey = this.isPromoCodeValid(promoCode)
+      ? customSubscriptionTypeKeys.trial
+      : customSubscriptionTypeKeys.common;
+
+    this.logger.info({ key: 'CreateOrderService.createOrder:subscriptionType', details: { userId, subscriptionTypeKey } });
+
+    const subscription = await this.startSubscriptionBusinessService.initiateSubscription({
+      subscriptionTypeKey,
+      userId,
+      cart: checkedCart,
+      userDetails,
+    });
+
+    /* * * * * * * * * * * */
+    /*                     */
+    /*   Create payment    */
+    /*                     */
+    /* * * * * * * * * * * */
+
+    const initPurchaseParams: InitPurchaseParams = {
+      userId,
+      goodId: subscription.id,
+      currencyIsoCode: 'EUR',
+      priceInCents: subscription.price.amount * 100,
+      acquiringProviderKey: acquiringProviderKeys.STRIPE_RECURSIVE_INTENT,
+    };
+
+
+    this.logger.info({ key: 'CreateOrderService.createOrder:initPurchaseParams', details: { userId, initPurchaseParams } });
+
+    const initiatePurchaseResult = await this.purchasesCommercialService
+      .initPurchase(initPurchaseParams);
+
+    this.logger.info({ key: 'CreateOrderService.createOrder:initPurchaseParams', details: { userId, initiatePurchaseResult } });
+
+    return initiatePurchaseResult;
+  }
+}
